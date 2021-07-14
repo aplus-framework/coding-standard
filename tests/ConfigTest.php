1c63c4d38c521929123e0417f06586fbd2472004
Natan Felles <natanfelles@gmail.com>
2021-07-14 11:04:09 -0300

    Update default header comments

--- tests/ConfigTest.php
+++ tests/ConfigTest.php
@@ -1,6 +1,6 @@
 <?php
 /*
- * This file is part of The Framework Coding Standard Library.
+ * This file is part of Aplus Framework Coding Standard Library.
  *
  * (c) Natan Felles <natanfelles@gmail.com>
  *
